ea6222eb7dee855c1f33f5574ae114a9f0781d67
Muhammed Hashim T <87515404+MuhammedHashimT@users.noreply.github.com>
2023-12-07 20:40:37 +0000

    names added

--- src/app/admin/programs/page.tsx
+++ src/app/admin/programs/page.tsx
@@ -1,7 +1,31 @@
-export default function Page(){
-    return(
-        <div>
-            <p>Hi</p>
-        </div>
-    )
+import Programs from "@/components/programs/Programs";
+import { Category, GetAllCandidateProgrammesDocument, GetAllCategoriesDocument, GetAllCategoriesQuery, GetAllCategoriesQueryVariables, GetAllProgrammesDocument, GetAllProgrammesQuery, GetAllProgrammesQueryVariables, Programme } from "@/gql/graphql";
+import { API_KEY } from "@/lib/env";
+import { getUrqlClient } from "@/lib/urql";
+
+export default async function Page() {
+  const { client } = getUrqlClient();
+
+  const programmes = await client.query<
+    GetAllProgrammesQuery,
+    GetAllProgrammesQueryVariables
+  >(GetAllProgrammesDocument, {
+    api_key: API_KEY,
+  });
+  console.log(programmes.data?.programmes);
+  
+  const categories = await client.query<
+    GetAllCategoriesQuery,
+    GetAllCategoriesQueryVariables
+  >(GetAllCategoriesDocument, {
+    api_key: API_KEY,
+  });
+
+
+
+  return (
+    <div>
+      {/* <Programs pageProps={1} categories={categories.data?.categories as Category[]}  programmes={programmes.data?.programmes as Programme[] } /> */}
+    </div>
+  );
 }
