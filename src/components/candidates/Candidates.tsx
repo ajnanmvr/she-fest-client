bce22c9d123109f01346ed12dcf51f2af9f5be49
Muhammed Hashim T <87515404+MuhammedHashimT@users.noreply.github.com>
2023-12-07 16:14:23 +0000

    models edited

--- src/components/candidates/Candidates.tsx
+++ src/components/candidates/Candidates.tsx
@@ -1,17 +1,26 @@
-"use client";
-import { Candidate } from "@/gql/graphql";
-import { useState } from "react";
+'use client';
+import { Candidate } from '@/gql/graphql';
+import { SERVER_URL } from '@/lib/urql';
+import { withUrqlClient } from 'next-urql';
+import { useState } from 'react';
+import { cacheExchange, fetchExchange } from 'urql';
+import CreateCandidate from './CreateCandidate';
+import UpdateCandidate from './UpdateCandidate';
 
 interface Props {
   candidates: Candidate[];
 }
-export default function Candidates(props: Props) {
-  const { candidates } = props;
-  const [searchTerm, setSearchTerm] = useState("");
-  const [selectedItem, setSelectedItem] = useState<Candidate | null>(null)
+function Candidates(props: Props) {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [candidates, setCandidates] = useState<Candidate[]>(props.candidates);
+  const [selectedItem, setSelectedItem] = useState<Candidate | null>(null);
+  const [isCreate, setIsCreate] = useState(false);
+  const [isUpdate, setIsUpdate] = useState(false);
+  const [isDelete, setIsDelete] = useState(false);
+  const [selected, setSelected] = useState<Candidate | null>(null);
   const filteredData = candidates.filter((candidate) => {
     return (
-      candidate?.name?.toLowerCase().includes(searchTerm.toLowerCase())||
+      candidate?.name?.toLowerCase().includes(searchTerm.toLowerCase()) ||
       candidate?.chestNO?.toLowerCase().includes(searchTerm.toLowerCase())
     );
   });
@@ -23,28 +32,37 @@ export default function Candidates(props: Props) {
             Candidate Search
           </h1>
           <div className="w-2/3 flex gap-2">
-          <input
-            type="text"
-            placeholder="Search by name or chest number.."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            className="w-full px-4 py-2 rounded-xl border-2 border-dashed border-primary"
-          />
-          <button className="bg-primary rounded-xl px-4 py-2 ">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-</svg>
-
-          </button>
+            <input
+              type="text"
+              placeholder="Search by name or chest number.."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="w-full px-4 py-2 rounded-xl border-2 border-dashed border-primary"
+            />
+            <button className="bg-primary rounded-xl px-4 py-2 ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 text-white"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </button>
           </div>
-
 
           <div className="flex flex-wrap gap-2 justify-center mt-3">
             {filteredData.map((candidate, index) => (
               <div
                 className="w-72 bg-secondary p-6 rounded-xl flex flex-col gap-2 items-start cursor-pointer"
                 key={index}
-                  onClick={() => setSelectedItem(candidate)}
+                onClick={() => setSelectedItem(candidate)}
               >
                 <h1 className="px-2 py-1 bg-primary inline rounded-lg text-white font-semibold">
                   {candidate.chestNO}
@@ -53,9 +71,21 @@ export default function Candidates(props: Props) {
               </div>
             ))}
           </div>
-      
         </div>
       </div>
+
+      <CreateCandidate />
+      <UpdateCandidate />
+      
     </>
   );
 }
+
+export default withUrqlClient(() => ({
+  url: SERVER_URL,
+  exchanges: [fetchExchange, cacheExchange],
+  fetchOptions: {
+    cache: 'no-cache',
+    credentials: 'include',
+  },
+}))(Candidates);
--- src/components/candidates/CreateCandidate.tsx
+++ src/components/candidates/CreateCandidate.tsx
@@ -1,7 +1,15 @@
-export default function CreateCandidate(){
-    return(
-        <div>
-            
-        </div>
-    )
+import React from 'react'
+
+interface Props {
+    isCreate : boolean
+    setIsCreate : React.Dispatch<React.SetStateAction<boolean>>;
+    
 }
+
+const CreateCandidate = () => {
+  return (
+    <div>CreateCandidate</div>
+  )
+}
+
+export default CreateCandidate
