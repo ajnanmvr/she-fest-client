4381beca93dba1561f663922a468ea615046e9d8
MUHAMMED HISHAM AC <122851090+hishamac@users.noreply.github.com>
2023-12-08 05:52:04 +0000

    Fix bug in login functionality
    Add validation for user input
    
    Refactor code for better performance
    
    Update README with new instructions
    
    Fix formatting issues in CSS
    
    Add error handling for database connection
    
    Update dependencies to latest versions
    
    Remove unused variables
    
    Fix broken links in navigation
    
    Add unit tests for new features

--- src/components/programs/ViewProgram.tsx
+++ src/components/programs/ViewProgram.tsx
@@ -1,93 +1,164 @@
 import { useGlobalContext } from "@/context/context";
 import {
-    AddCandidateDocument,
-    AddCandidateMutation,
-    AddCandidateMutationVariables,
-    AddProgrammeDocument,
-    AddProgrammeMutation,
-    AddProgrammeMutationVariables,
-    Candidate,
-    Category,
-    Mode,
-    Programme,
-    Roles,
-    Team,
-    Type,
-  } from "@/gql/graphql";
-  import React from "react";
-  import { OperationResult, useMutation } from "urql";
-  
-  interface Props {
-    isView: boolean;
-    setIsView: React.Dispatch<React.SetStateAction<boolean>>;
-    programs: Programme[];
-    setPrograms: React.Dispatch<React.SetStateAction<Programme[]>>;
-    categories: Category[];
-    selected: Programme;
-  }
-  
-  const ViewProgram = (props: Props) => {
-    const [state, ViewProgramExecute] = useMutation(AddProgrammeDocument);
-    const [name, setName] = React.useState<string>("");
-    const [candidateCount, setCandidateCount] = React.useState<number>(0);
-    const [category, setCategory] = React.useState<string>("");
-    const [duration, setDuration] = React.useState<number>(0);
-    const [groupCount, setGroupCount] = React.useState<number>(0);
-    const [programCode, setProgramCode] = React.useState<string>("");
-    const [type, setType] = React.useState<string>("");
-    const [mode, setMode] = React.useState<string>("");
-    const [conceptNote, setConceptNote] = React.useState<string>("");
-    const { data, setData } = useGlobalContext();
+  AddCandidateDocument,
+  AddCandidateMutation,
+  AddCandidateMutationVariables,
+  AddProgrammeDocument,
+  AddProgrammeMutation,
+  AddProgrammeMutationVariables,
+  Candidate,
+  Category,
+  Mode,
+  Programme,
+  Roles,
+  Team,
+  Type,
+} from "@/gql/graphql";
+import React from "react";
+import { OperationResult, useMutation } from "urql";
 
-    const HandleSubmit = async () => {
-      const datas: OperationResult<
-        AddProgrammeMutation,
-        AddProgrammeMutationVariables
-      > = await ViewProgramExecute({
-        name: name,
-        category: category,
-        candidateCount: candidateCount,
-        duration: duration,
-        groupCount: groupCount,
-        programCode: programCode,
-        type: type as Type,
-        mode: mode as Mode,
-        conceptNote: conceptNote,
-      });
-      console.log(datas);
-  
-      if (datas.data?.createProgramme) {
-        props.setPrograms([...props.programs as Programme[], datas.data.createProgramme as Programme]);
-        props.setIsView(false);
-      }
-    };
-  
-    return (
-      <div
-        className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center  items-center  ${
-          props.isView ? "block" : "hidden"
-        } `}
-      >
+interface Props {
+  isView: boolean;
+  setIsView: React.Dispatch<React.SetStateAction<boolean>>;
+  programs: Programme[];
+  setPrograms: React.Dispatch<React.SetStateAction<Programme[]>>;
+  categories: Category[];
+  selected: Programme;
+}
 
-        
-        <div className="bg-white p-3 rounded-xl flex flex-col items-center max-w-[400px] text-center">
-        <p className="text-sm mt-3 font-bold text-primary">Candidates</p>
-        {
-            data && data.admin?.roles  == Roles.Admin || Roles.Controller ? (
+const ViewProgram = (props: Props) => {
+  const [state, ViewProgramExecute] = useMutation(AddProgrammeDocument);
+  const [name, setName] = React.useState<string>("");
+  const [candidateCount, setCandidateCount] = React.useState<number>(0);
+  const [category, setCategory] = React.useState<string>("");
+  const [duration, setDuration] = React.useState<number>(0);
+  const [groupCount, setGroupCount] = React.useState<number>(0);
+  const [programCode, setProgramCode] = React.useState<string>("");
+  const [type, setType] = React.useState<string>("");
+  const [mode, setMode] = React.useState<string>("");
+  const [conceptNote, setConceptNote] = React.useState<string>("");
+  const { data, setData } = useGlobalContext();
+
+  const HandleSubmit = async () => {
+    const datas: OperationResult<
+      AddProgrammeMutation,
+      AddProgrammeMutationVariables
+    > = await ViewProgramExecute({
+      name: name,
+      category: category,
+      candidateCount: candidateCount,
+      duration: duration,
+      groupCount: groupCount,
+      programCode: programCode,
+      type: type as Type,
+      mode: mode as Mode,
+      conceptNote: conceptNote,
+    });
+    console.log(datas);
+
+    if (datas.data?.createProgramme) {
+      props.setPrograms([
+        ...(props.programs as Programme[]),
+        datas.data.createProgramme as Programme,
+      ]);
+      props.setIsView(false);
+    }
+  };
+
+  return (
+    <div
+      className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center  items-center  ${
+        props.isView ? "block" : "hidden"
+      } `}
+    >
+      <div className="bg-white p-3 rounded-xl flex flex-col items-center min-w-[400px]  max-w-[400px] max-h-screen text-center ">
+        {/* {data && data.admin?.roles == Roles.Admin || Roles.Controller  && (
+          <>
+            {
+              // props.selected?.candidateProgramme?.map((candidate, index) => (
+              //   <div className="border-2 border-primary rounded-lg p-3 my-2 w-full flex justify-between items-center">
+              //     <p className="text-primary font-semibold">{candidate.candidate?.name}</p>
+              //     <p className="text-primary font-semibold">{candidate.candidate?.team?.name}</p>
+              //   </div>
+              // ))
               <>
-             {
-              props.selected?.candidateProgramme?.map((candidate, index) => (
-                <div className="border-2 border-primary rounded-lg p-3 my-2 w-full flex justify-between items-center">
-                  <p className="text-primary font-semibold">{candidate.candidate?.name}</p>
-                  <p className="text-primary font-semibold">{candidate.candidate?.team?.name}</p>
+                <p className="text-lg mt-3 font-bold text-primary">
+                  Candidates
+                </p>
+                <div className="w-full   overflow-y-auto">
+                  <div className="border-2 border-primary rounded-lg p-3 my-2 w-full justify-between items-center">
+                    <p className="text-primary font-bold">Muhammed Hashim</p>
+                    <p className="text-primary font-semibold">
+                      Vahdiyya Kondotty
+                    </p>
+                  </div>
+                  <div className="border-2 border-primary rounded-lg p-3 my-2 w-full justify-between items-center">
+                    <p className="text-primary font-bold">Muhammed Hashim</p>
+                    <p className="text-primary font-semibold">
+                      Vahdiyya Kondotty
+                    </p>
+                  </div>{" "}
+                  <div className="border-2 border-primary rounded-lg p-3 my-2 w-full justify-between items-center">
+                    <p className="text-primary font-bold">Muhammed Hashim</p>
+                    <p className="text-primary font-semibold">
+                      Vahdiyya Kondotty
+                    </p>
+                  </div>{" "}
+                  <div className="border-2 border-primary rounded-lg p-3 my-2 w-full justify-between items-center">
+                    <p className="text-primary font-bold">Muhammed Hashim</p>
+                    <p className="text-primary font-semibold">
+                      Vahdiyya Kondotty
+                    </p>
+                  </div>{" "}
+                  <div className="border-2 border-primary rounded-lg p-3 my-2 w-full justify-between items-center">
+                    <p className="text-primary font-bold">Muhammed Hashim</p>
+                    <p className="text-primary font-semibold">
+                      Vahdiyya Kondotty
+                    </p>
+                  </div>
+                  <div className="border-2 border-primary rounded-lg p-3 my-2 w-full justify-between items-center">
+                    <p className="text-primary font-bold">Muhammed Hashim</p>
+                    <p className="text-primary font-semibold">
+                      Vahdiyya Kondotty
+                    </p>
+                  </div>
+                  <div className="border-2 border-primary rounded-lg p-3 my-2 w-full justify-between items-center">
+                    <p className="text-primary font-bold">Muhammed Hashim</p>
+                    <p className="text-primary font-semibold">
+                      Vahdiyya Kondotty
+                    </p>
+                  </div>
+                  <div className="border-2 border-primary rounded-lg p-3 my-2 w-full justify-between items-center">
+                    <p className="text-primary font-bold">Muhammed Hashim</p>
+                    <p className="text-primary font-semibold">
+                      Vahdiyya Kondotty
+                    </p>
+                  </div>
+                  <div className="border-2 border-primary rounded-lg p-3 my-2 w-full justify-between items-center">
+                    <p className="text-primary font-bold">Muhammed Hashim</p>
+                    <p className="text-primary font-semibold">
+                      Vahdiyya Kondotty
+                    </p>
+                  </div>
                 </div>
-              ))
-
-             }
               </>
-            ) : <></>
+            }
+          </>
+        )} */}
 
-        }
+        {data && data.admin?.roles == Roles.TeamManager && (
+          <div>
+            <div className="border-2 border-primary rounded-lg p-3 my-2 w-full justify-between items-center">
+              <p className="text-primary font-bold">Muhammed Hashim</p>
+              <p className="text-primary font-semibold">Vahdiyya Kondotty</p>
+            </div>
+            <div className="border-2 border-primary rounded-lg p-3 my-2 w-full justify-between items-center">
+              <p className="text-primary font-bold">Muhammed Hashim</p>
+              <p className="text-primary font-semibold">Vahdiyya Kondotty</p>
+            </div>
+          </div>
+        )}
+        {data && data.admin?.roles == Roles.TeamManager && (
           <form
             onSubmit={(e) => {
               e.preventDefault();
@@ -95,6 +166,9 @@ import {
             }}
             className={`p-3 text-left`}
           >
+            <p className="text-lg mt-3 font-bold text-primary text-center">
+              Add Candidates
+            </p>
             <p className="text-sm mt-3 font-bold text-primary">Name</p>
             <input
               type="text"
@@ -116,21 +190,21 @@ import {
                 </option>
               ))}
             </select>
-            
+
             <button className="w-full bg-primary text-white font-bold px-3 py-2 rounded-lg mt-3">
               Submit
             </button>
           </form>
-          <button
-            className="bg-red-700 text-white font-bold px-3 py-2 rounded-lg"
-            onClick={() => props.setIsView(false)}
-          >
-            Close
-          </button>
-        </div>
+        )}
+        <button
+          className="bg-red-700 text-white font-bold px-3 py-2 rounded-lg mt-3"
+          onClick={() => props.setIsView(false)}
+        >
+          Close
+        </button>
       </div>
-    );
-  };
-  
-  export default ViewProgram;
-  
+    </div>
+  );
+};
+
+export default ViewProgram;
