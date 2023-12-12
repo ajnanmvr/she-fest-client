7aba6571870776876efb56ee8e1f20ebba717aef
Your Name <you@example.com>
2023-12-12 14:13:58 +0530

    Add candidate ID to candidateProgramme query

--- src/components/programs/ViewProgram.tsx
+++ src/components/programs/ViewProgram.tsx
@@ -6,11 +6,11 @@ import {
   Candidate,
   CandidateProgramme,
   Category,
-  Mode,
+  DeleteCandidateProgrammeDocument,
+  DeleteCandidateProgrammeMutation,
+  DeleteCandidateProgrammeMutationVariables,
   Programme,
   Roles,
-  Team,
-  Type,
   Zone,
 } from '@/gql/graphql';
 import React, { useEffect } from 'react';
@@ -28,34 +28,30 @@ interface Props {
   zones: Zone[];
 }
 
-const   ViewProgram = (props: Props) => {
+const ViewProgram = (props: Props) => {
   const [state, ViewProgramExecute] = useMutation(
     AddCandidateProgrammeDocument
+  );
+  const [ status , deleteCPExicute] = useMutation(
+    DeleteCandidateProgrammeDocument
   );
   const [name, setName] = React.useState<string>('');
   const [chestNo, setChestNo] = React.useState<string>('');
   const [zone, setZone] = React.useState<string>('');
+  const [toDeleteCP , setToDeleteCP] = React.useState<CandidateProgramme>()
   const [candidates, setCandidates] = React.useState<Candidate[]>(
     props.candidates
   );
   const [zones, setZones] = React.useState<Zone[]>(props.zones);
-  const [candidateCount, setCandidateCount] = React.useState<number>(0);
-  const [category, setCategory] = React.useState<string>("");
-  const [duration, setDuration] = React.useState<number>(0);
-  const [groupCount, setGroupCount] = React.useState<number>(0);
-  const [programCode, setProgramCode] = React.useState<string>("");
-  const [type, setType] = React.useState<string>("");
-  const [mode, setMode] = React.useState<string>("");
-  const [conceptNote, setConceptNote] = React.useState<string>("");
-  const [selectedCandidateProgramme,setSelectedCandidateProgramme] = React.useState<CandidateProgramme[]>(props.selected.candidateProgramme as CandidateProgramme[])
+  const [selectedCandidateProgramme, setSelectedCandidateProgramme] = React.useState<CandidateProgramme[]>(props.selected.candidateProgramme as CandidateProgramme[])
   let filteredCandidate = candidates?.find((candidate) => {
     return candidate?.chestNO?.toLowerCase() == chestNo.toLowerCase();
   });
   const { data, setData } = useGlobalContext();
 
   useEffect(() => {
-  console.log(selectedCandidateProgramme);
-  
+    console.log(selectedCandidateProgramme);
+
   }, [])
 
   const HandleSubmit = async () => {
@@ -69,30 +65,26 @@ const   ViewProgram = (props: Props) => {
     console.log(datas);
 
     if (datas.data?.createCandidateProgramme) {
-      setSelectedCandidateProgramme([...selectedCandidateProgramme,datas.data.createCandidateProgramme as CandidateProgramme])
+      setSelectedCandidateProgramme([...selectedCandidateProgramme, datas.data.createCandidateProgramme as CandidateProgramme])
       console.log(selectedCandidateProgramme);
-      
+
       // props.setIsView(false);
     }
-    // if (datas.data?.createCandidateProgramme) {
-    //   const newProgramme = datas.data?.createCandidateProgramme as CandidateProgramme;
-    //   const existingProgramIndex = props.programs.findIndex(
-    //     (program) => program.id === newProgramme.programme?.id
-    //   );
 
-    //   if (existingProgramIndex !== -1) {
-    //     // Update existing program
-    //     const updatedPrograms = [...props.programs];
-    //     updatedPrograms[existingProgramIndex].candidateProgramme?.push(newProgramme)
-    //     props.setPrograms(updatedPrograms);
-    //   } else {
-    //     // Add new program
-    //     props.setPrograms([...props.programs]);
-    //   }
 
-    //   props.setIsView(false);
-    // }
+  };
 
+  const HandleDelete = async () => {
+
+    console.log(toDeleteCP);
+    
+    // delete candidate from program
+    const datas: OperationResult<
+      DeleteCandidateProgrammeMutation,
+      DeleteCandidateProgrammeMutationVariables
+    > = await deleteCPExicute({
+      id: toDeleteCP?.id as number
+    });
   };
 
   return (
@@ -185,6 +177,25 @@ const   ViewProgram = (props: Props) => {
                     <p className="text-primary font-semibold">
                       {cp.candidate?.team?.name}
                     </p>
+                    <button onClick={async() => {
+                      setToDeleteCP(cp)
+                     await HandleDelete()
+                    }} className="bg-white border border-dashed border-primary rounded-xl px-4 py-2 ">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6 text-red-600"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                        />
+                      </svg>
+                    </button>
                   </div>
                 );
               }
